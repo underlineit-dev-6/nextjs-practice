a36842cb7d590c6332f2a53696cebac7d844d137
chaitanya <chaitanya@underlineit.co.in>
2023-10-13 09:50:24 +0530

    feat: understanding dynamic routes

---
+++ nextjs-auth/src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import SignInForm from "@/app/components/forms/SignInForm";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className=" w-full">
+      <SignInForm />
+    </div>
+  );
+};
+
+export default page;
---
+++ nextjs-auth/src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const page = () => {
+  return <div className="bg-blue-400 w-full">
+    Signup
+  </div>;
+};
+
+export default page;
